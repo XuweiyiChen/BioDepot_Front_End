4e69f6fbdd570e7b798ee40df34aca8f7a2eb62b
XuweiyiChen <chenxuweiyi@gmail.com>
2022-02-14 02:20:12 -0800

    vue vite enabled

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="background">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  #background {
+    background-image: url('./assets/bg.png');
+    height: 800px;
+    width: 1024px;
+  }
+</style>
