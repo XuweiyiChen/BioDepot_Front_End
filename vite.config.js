6c50f93816fbf1cf937f2fda9b8a53b08cf78daa
XuweiyiChen <chenxuweiyi@gmail.com>
2022-02-22 03:43:00 -0800

    structure added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
   server: {
     port: 8080
   },
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    }),
+  ],
   base: '',
   build: {
   	outDir: 'dist'
